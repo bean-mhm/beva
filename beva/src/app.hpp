369745a869c79dcd3a364747ce510658dbe7a9d9
bean <harry.bean.dev@gmail.com>
2024-08-26 23:05:39 +0000

    added alignment specifications in UniformBufferObject

--- beva/src/app.hpp
+++ beva/src/app.hpp
@@ -21,9 +21,9 @@ namespace beva_demo
 
     struct UniformBufferObject
     {
-        glm::mat4 model;
-        glm::mat4 view;
-        glm::mat4 proj;
+        alignas(16) glm::mat4 model;
+        alignas(16) glm::mat4 view;
+        alignas(16) glm::mat4 proj;
     };
 
     struct Vertex
